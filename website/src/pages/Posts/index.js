5df132478781afd2ae183d47dd23ce2f15de1ffa
bianca-maria <bianca.engcomp@gmail.com>
2022-11-29 18:15:15 +0000

    consumindo o serviço da listagem de posts

--- website/src/pages/Posts/index.js
+++ website/src/pages/Posts/index.js
@@ -1,15 +1,39 @@
-import { Typography } from 'antd';
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
-import _service from '@netuno/service-client';
-import _auth from '@netuno/auth-client';
+import _service from "@netuno/service-client";
+import _auth from "@netuno/auth-client";
+import Post from "./Post";
 
-const { Title } = Typography;
 
 function Posts() {
-    if (_auth.isLogged()) {     
-        return <Title level={2}>Hello World</Title>;
-    }
-    return (<Navigate to="/login" />);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = () => {
+    _service({
+      url: "post/list",
+      success: (response) => {
+        setPosts(response.json);
+      },
+      fail: (e) => {
+        console.log("Service Error", e);
+      },
+    });
+  };
+
+  if (!_auth.isLogged()) {
+    return <Navigate to="/login" />;
+  }
+  return (
+    <div>
+        {
+          posts.map((post) => <Post {...post}/>)
+        }
+    </div>
+  );
 }
 
 export default Posts;
