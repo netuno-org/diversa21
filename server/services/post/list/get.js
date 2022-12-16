b30f12357db8c80120b9343afeec490acb3e9795
bianca-maria <bianca.engcomp@gmail.com>
2022-12-16 13:07:26 +0000

    modificação na query da listagem do post/publicação

--- server/services/post/list/get.js
+++ server/services/post/list/get.js
@@ -1,11 +1,18 @@
+const parent = _req.getString('parent');
+let dbParent = _val.map();
+
+if (parent != '') {
+    dbParent = _db.get('post', parent);
+}
+
 const dbPosts = _db.query(`
     SELECT post.uid, post.moment, post.content, 
         people.name AS "people_name", people.uid AS "people_uid", 
         people.avatar AS "people_avatar"
     FROM post
         INNER JOIN people ON post.people_id = people.id
-    WHERE post.parent_id IS NULL OR post.parent_id = 0
-`);
+    WHERE post.parent_id IS NULL OR post.parent_id = ?::int
+`, dbParent.getInt('id', 0));
 const posts = _val.list();
 for (const dbPost of dbPosts) {
     posts.add(
