8ae7102cf1bb79c2f3d527a77ed5142a02b7de80
bianca-maria <bianca.engcomp@gmail.com>
2022-12-16 13:34:57 +0000

    inclusão do botão responder

--- website/src/components/Post/index.js
+++ website/src/components/Post/index.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import _service from "@netuno/service-client";
-import { Comment, Card, Avatar } from "antd";
+import { Comment, Card, Avatar, Button } from "antd";
+import Editor from "./Editor";
+import "./index.less";
 
 function Post({ uid, moment, content, people }) {
   const [avatarUrl, setAvatarUrl] = useState("/images/profile-default.png");
+  const [showEditor, setShowEditor] = useState(false);
   useEffect(() => {
     if (people.avatar) {
       setAvatarUrl(
@@ -13,10 +16,24 @@ function Post({ uid, moment, content, people }) {
       );
     }
   }, []);
+  const onCreated = (post) => {
+    console.log(post);
+  };
   return (
-    <Card>
+    <Card className="post-container">
       <Comment
-        actions={[<span key="comment-nested-reply-to">Reply to</span>]}
+        actions={[
+          showEditor ? (
+            <Editor
+              type="comment"
+              onCancel={() => setShowEditor(false)}
+              onCreated={onCreated}
+              parent={uid}
+            />
+          ) : (
+            <Button onClick={() => setShowEditor(true)}>Responder</Button>
+          ),
+        ]}
         author={people.name}
         datetime={moment}
         avatar={<Avatar src={avatarUrl} alt={people.name} />}
