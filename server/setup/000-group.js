c5607ffffc6c7b1b0342095bc8c5608092294cb3
Eduardo Velasques <eduveks@gmail.com>
2026-04-07 10:48:57 +0100

    All groups added.

--- server/setup/000-group.js
+++ server/setup/000-group.js
@@ -1,5 +1,25 @@
+import {_val, _group} from "@netuno/server-types"
+
 _group.createIfNotExists(
     _val.map()
         .set("name", "Pessoa")
         .set("code", "people")
+)
+
+_group.createIfNotExists(
+    _val.map()
+        .set("name", "Revisão")
+        .set("code", "review")
+)
+
+_group.createIfNotExists(
+    _val.map()
+        .set("name", "Gestão")
+        .set("code", "management")
+)
+
+_group.createIfNotExists(
+    _val.map()
+        .set("name", "Super Administrador")
+        .set("code", "super-admin")
 )
