40a17839e196a27f26b41a9d3a7c8be815661dfd
thiago.dev2 <thiago.dev2@gmail.com>
2026-04-14 18:29:56 -0300

    add bithdate in my profile and deleted de btn myposts

--- website/src/pages/ReservedArea/profile/MyProfile/index.jsx
+++ website/src/pages/ReservedArea/profile/MyProfile/index.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Spin } from 'antd';
-import { Avatar, Card, Button } from 'antd';
+import { Avatar, Card, Button, Typography } from 'antd';
+import { UserOutlined, EnvironmentOutlined, CalendarOutlined } from '@ant-design/icons';
 import _service from '@netuno/service-client';
 
 const { Meta } = Card;
 const { Text } = Typography;
 
-
-function MyProfile({loggedUserInfo}) {
-
+function MyProfile({ loggedUserInfo }) {
     const [avatarUrl, setAvatarUrl] = useState("/images/profile-default.png");
+
+    const [year, mount, day] = loggedUserInfo.birthDate.split('-');
+    const birthDate = `${day}/${mount}/${year}`;
 
     useEffect(() => {
         if (loggedUserInfo.avatar) {
@@ -19,26 +20,25 @@ function MyProfile({loggedUserInfo}) {
         }
     }, [loggedUserInfo]);
 
-
     return (
-    <Card className="my-profile"
-        style={{ width: '70%' }}
-    >
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-            <Meta
-                style={{ height: '150px', textAlign: 'center' }}
-                avatar={<Avatar src={avatarUrl} style={{ width: '100%', height: "100%" }} />}
-            />
-            <div style={{ display: 'flex', flexDirection: 'column'  }}>
-                <Text>{loggedUserInfo.name}</Text>
-                <Text>{loggedUserInfo.city}, {loggedUserInfo.state}</Text>
+        <Card className="my-profile"
+            style={{ width: '70%' }}
+        >
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Meta
+                    style={{ height: '150px', textAlign: 'center' }}
+                    avatar={<Avatar src={avatarUrl} style={{ width: '100%', height: "100%" }} />}
+                />
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Text><UserOutlined /> {loggedUserInfo.name}</Text>
+                    <Text><EnvironmentOutlined /> {loggedUserInfo.city}, {loggedUserInfo.state}</Text>
+                    <Text><CalendarOutlined /> {birthDate}</Text>
+                </div>
+                <div style={{ display: 'flex', gap: '10px', marginLeft: 'auto' }}>
+                    <Button type="primary">Instituição</Button>
+                </div>
             </div>
-            <div style={{ display: 'flex', gap: '10px', marginLeft: 'auto' }}>
-            <Button  type="primary">Instituição</Button>
-            <Button  type="primary">Meus posts</Button>
-            </div>
-        </div>
-    </Card>
+        </Card>
     )
 }
 export default MyProfile;
