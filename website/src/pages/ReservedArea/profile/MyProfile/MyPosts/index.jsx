990a58bda8d1a8214e61ad6feb02d38bdeccfee2
thiago.dev2 <thiago.dev2@gmail.com>
2026-04-21 18:05:37 -0300

    adjusted code in myPosts

--- website/src/pages/ReservedArea/profile/MyProfile/MyPosts/index.jsx
+++ website/src/pages/ReservedArea/profile/MyProfile/MyPosts/index.jsx
@@ -5,88 +5,97 @@ import { Tabs,  Pagination  } from 'antd';
 import Post from '../../../../../components/Post/';
 
 function MyPosts({ loggedUserInfo }) {
-
-  const [posts, setPosts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  
-  const startIndex = (currentPage - 1) * 3;
-  const endIndex = startIndex + 3;
-  const paginatedPosts = posts.slice(startIndex, endIndex);
-
-  useEffect(() => {
-    if (!loggedUserInfo?.uid) return;
-    _service({
-      method: 'GET',
-      url: "post",
-      data: { authorUid: loggedUserInfo.uid },
-      success: (response) => {
-        if (response.json) {
-          setPosts(response.json);
-          setCurrentPage(1);
-        }
-      },
-      fail: (error) => {
-        console.log("Service Error", error);
-      }
+    const [allPosts, setAllPosts] = useState([]);
+    const [paginatedPosts, setPaginatedPosts] = useState([]);
+    const [pagination, setPagination] = useState({
+        current: 1,
+        size: 3
     });
 
-  }, [loggedUserInfo?.uid]);
+    useEffect(() => {
+        if (!loggedUserInfo?.uid) {
+            return;
+        }
+        _service({
+            method: 'GET',
+            url: "post",
+            data: { authorUid: loggedUserInfo.uid },
+            success: (response) => {
+                if (response.json) {
+                    setAllPosts(response.json);
+                    setPagination({...pagination, current: 1});
+                }
+            },
+            fail: (error) => {
+                console.log("Service Error", error);
+            }
+        });
 
-  const onRemovePost = (uid) => {
-    setPosts((currentPosts) => currentPosts.filter((post) => post.uid !== uid));
-  };
+    }, [loggedUserInfo?.uid]);
 
-  const onEditPost = (uid, content) => {
-    setPosts((currentPosts) =>
-      currentPosts.map((post) =>
-        post.uid === uid ? { ...post, content } : post
-      )
-    );
-  };
+    useEffect(() => {
+        const {current} = pagination;
+        const startIndex = (current - 1) * 3;
+        const endIndex = startIndex + 3;
+        const posts = allPosts.slice(startIndex, endIndex);
+        setPaginatedPosts(posts);
+    }, [pagination]);
 
-  const tabItems = [
-    {
-      key: '1',
-      label: 'Meus posts',
-      children: posts.length > 0 ? (
-        <>
-          {paginatedPosts.map((post) => (
-            <Post
-              key={post.uid}
-              loggedUserInfo={loggedUserInfo}
-              onRemovePost={onRemovePost}
-              onEditPost={onEditPost}
-              {...post}
-            />
-          ))}
-          <Pagination
+    const onRemovePost = (uid) => {
+        setAllPosts((currentPosts) => currentPosts.filter((post) => post.uid !== uid));
+    };
+
+    const onEditPost = (uid, content) => {
+        setAllPosts((currentPosts) =>
+            currentPosts.map((post) =>
+                post.uid === uid ? { ...post, content } : post
+            )
+        );
+    };
+
+    const tabItems = [
+        {
+            key: '1',
+            label: 'Meus posts',
+            children: allPosts.length > 0 ? (
+                <>
+                    {paginatedPosts.map((post) => (
+                        <Post
+                            key={post.uid}
+                            loggedUserInfo={loggedUserInfo}
+                            onRemovePost={onRemovePost}
+                            onEditPost={onEditPost}
+                            {...post}
+                        />
+                    ))}
+                    <Pagination
+                        style={{ marginTop: '20px' }}
+                        align='center'
+                        total={allPosts.length}
+                        current={pagination.current}
+                        pageSize={pagination.size}
+                        onChange={(current) => setPagination({...pagination, current})}
+                    />
+                </>
+            ) : (
+                <p>Nenhum post encontrado</p>
+            ),
+            icon: <MessageOutlined />,
+        },
+        {
+            key: '2',
+            label: 'Minhas participações em posts',
+            children: 'Minhas participações em posts',
+            icon: <EditOutlined />,
+        },
+    ];
+
+    return (
+        <Tabs
             style={{ marginTop: '20px' }}
-            align='center'
-            total={posts.length}
-            current={currentPage}
-            pageSize={3}
-            onChange={setCurrentPage}
-          />
-        </>
-      ) : (
-        <p>Nenhum post encontrado</p>
-      ),
-      icon: <MessageOutlined />,
-    },
-    {
-      key: '2',
-      label: 'Minhas participações em posts',
-      children: 'Minhas participações em posts',
-      icon: <EditOutlined />,
-    },
-  ];
-
-  return (
-    <Tabs
-      style={{ marginTop: '20px' }}
-      defaultActiveKey="1"
-      items={tabItems}
-    />
-  );
+            defaultActiveKey="1"
+            items={tabItems}
+        />
+    );
 }
 export default MyPosts;
