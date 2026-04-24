8fe051a16403010f7f9643ff63a5614f60f3fee2
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-04-24 16:04:26 -0300

    Style: 2 spaces indentation

--- server/services/post/like/post.js
+++ server/services/post/like/post.js
@@ -4,45 +4,45 @@ const postUid = _req.getString('uid');
 const dbPost = _db.get('post', postUid);
 
 const peopleId = _db.queryFirst(`
-    SELECT id
-    FROM people 
-    WHERE people_user_id = ?::int
+  SELECT id
+  FROM people 
+  WHERE people_user_id = ?::int
 `, _user.id).getInt("id");
 
 if (!dbPost) {
-    _header.status(400);
-    _exec.stop();
+  _header.status(400);
+  _exec.stop();
 }
 
 const dbLike = _db.queryFirst(`
-    SELECT id 
-    FROM post_like
-    WHERE 1 = 1
-      AND post_id = ?::int
-      AND people_id = ?::int
+  SELECT id 
+  FROM post_like
+  WHERE 1 = 1
+    AND post_id = ?::int
+    AND people_id = ?::int
 `, dbPost.getInt('id'), peopleId);
 
 if (dbLike) {
-    _header.status(400);
-    _exec.stop();
+  _header.status(400);
+  _exec.stop();
 }
 
 _db.insert(
-    'post_like',
-    _val.map()
-        .set('post_id', dbPost.getInt('id'))
-        .set('people_id', peopleId)
-        .set('moment', _db.timestamp())
+  'post_like',
+  _val.map()
+    .set('post_id', dbPost.getInt('id'))
+    .set('people_id', peopleId)
+    .set('moment', _db.timestamp())
 );
 
 _db.update(
-    'post',
-    dbPost.getInt('id'),
-    _val.map()
-        .set('likes', dbPost.getInt('likes') + 1)
+  'post',
+  dbPost.getInt('id'),
+  _val.map()
+    .set('likes', dbPost.getInt('likes') + 1)
 );
 
 _out.json(
-    _val.map()
-        .set('result', true)
+  _val.map()
+    .set('result', true)
 );
