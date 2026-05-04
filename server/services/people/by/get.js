ed034247d36dd02e017ddf62077391dee1f0653d
Eduardo Velasques <eduveks@gmail.com>
2026-05-04 17:08:26 +0100

    A people service uses the core people.

--- server/services/people/by/get.js
+++ server/services/people/by/get.js
@@ -1,50 +1,29 @@
 import {_req, _db, _val, _header, _exec, _out} from "@netuno/server-types"
 
-const username = _req.getString("username");
+import people from "#core/lib/people.js";
 
-const dbPeople = _db.queryFirst(`
-    SELECT people.uid,
-        people.name,
-        netuno_user.user,
-        people.email,
-        people.avatar,
-        people.birth_date,
-        city.name AS "city",
-        state.name AS "state",
-        country.name AS "country",
-        institution.uid AS "institution",
-        netuno_group.code AS "group"
-    FROM people 
-        INNER JOIN netuno_user ON people.people_user_id = netuno_user.id 
-        INNER JOIN city ON people.city_id = city.id
-        INNER JOIN state ON city.state_id = state.id
-        INNER JOIN country ON state.country_id = country.id
-        INNER JOIN institution on people.institution_id = institution.id
-        INNER JOIN netuno_group ON netuno_user.group_id = netuno_group.id
-    WHERE netuno_user."user" = ?::varchar;
-`, username);
-
-if (!dbPeople) {
+const onAbort = () => {
   _header.status(404)
   _out.json(
     _val.map()
-    .set("error", "not-exist")
+      .set("error", "not-exist")
   );
   _exec.stop()
+};
+
+const username = _req.getString("username");
+
+const dbPeople = people.getByUsername(username);
+
+if (!dbPeople) {
+  onAbort();
 }
 
-const data = _val.map()
-  .set("uid", dbPeople.getString("uid"))
-  .set("name", dbPeople.getString("name"))
-  .set("email", dbPeople.getString("email"))
-  .set("avatar", dbPeople.getString("avatar") !== '')
-  .set("birthDate", dbPeople.getString("birth_date"))
-  .set("city", dbPeople.getString("city"))
-  .set("state", dbPeople.getString("state"))
-  .set("country", dbPeople.getString("country"))
-  .set("institution", dbPeople.getString("institution"))
-  .set("username", dbPeople.getString("user"))
-  .set("group", dbPeople.getString("group"))
+const data = people.getData(dbPeople.getUID("uid"));
+
+if (!data) {
+  onAbort();
+}
 
 _out.json(
   _val.map()
