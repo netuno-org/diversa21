4e0f7e67041d9173f27f7306e44420a610e45066
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2026-05-04 17:11:46 +0100

    feat: search for city
    - returns country > state > city

---
+++ server/services/location/city/search/get.js
@@ -0,0 +1,26 @@
+import { _req, _db, _out } from "@netuno/server-types";
+
+const input = _req.getString('city') || "";
+
+const query = `
+SELECT * FROM (
+   SELECT
+       city.uid AS id,
+       country.name || ' > ' || state.name || ' > ' || city.name AS label
+   FROM city
+   INNER JOIN state ON city.state_id = state.id
+   INNER JOIN country ON state.country_id = country.id
+   WHERE city.name ILIKE ?::text
+) AS results
+LIMIT 10
+`;
+
+const result = _db.query(
+  query,
+  "%" + input + "%"
+);
+
+_out.json({
+  result,
+  success: true
+})
