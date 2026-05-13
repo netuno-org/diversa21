4eb3f31b7eae819e490dfc4d092b0362e1ca1f95
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-05-13 13:19:08 -0300

    refactor: use constant and helper functions

--- server/core/lib/permissions.js
+++ server/core/lib/permissions.js
@@ -6,55 +6,56 @@ const data = people.getData(peopleUid);
 const loggedUserGroupCode = data.getValues("group").getString("code");
 const loggedUserInstitutionUid = data.getValues("institution").getUID("uid");
 
-export default {
-  canCreateAnyUser: () => {
-    return loggedUserGroupCode === "super-admin"; 
-  },
-  // canManageUser: (people) => {
-  //   return (
-  //     (
-  //       loggedUserGroupCode === "management" && 
-  //       people.group.code === "member" &&
-  //       loggedUserInstitutionUid === people.institution.uid
-  //     ) || (
-  //       loggedUserGroupCode === "super-admin"
-  //     )
-  //   );
-  // },
-  // canChangeUserGroup: () => {
-  //   return loggedUserGroupCode === "super-admin"; 
-  // },
-  // canChangeUserInstitution: () => {
-  //   return loggedUserGroupCode === "super-admin"; 
-  // },
-  // canChangeOwnInstitution: () => {
-  //   return loggedUserGroupCode === "super-admin"; 
-  // },
-  canCreateMember: (newMemberGroupCode, institutionUid) => {
+const SUPER_ADMIN = "super-admin";
+const REVIEW = "review";
+const MANAGEMENT = "management";
+const MEMBER = "member";
+
+const isLoggedUserGroup = (groupCode) => {
+  const loggedUserGroupCode = _group.code();
+  return loggedUserGroupCode === groupCode; 
+}
+
+const isLoggedUserInstitution = (institutionUid) => {
+  const peopleUid = people.getLogged().getUID("uid");
+  const data = people.getData(peopleUid);
+  const loggedUserInstitutionUid = data.getValues("institution").getUID("uid");
+  return loggedUserInstitutionUid.equals(institutionUid);
+}
+
+const canManageUserOrCreateMember = (otherUserGroupCode, otherUserInstitutionUid) => {
     return (
       (
-        loggedUserGroupCode === "super-admin"
+        isLoggedUserGroup(SUPER_ADMIN)
       ) || (
-        loggedUserGroupCode === "management" &&
-        loggedUserInstitutionUid.equals(institutionUid) &&
-        newMemberGroupCode === "member" 
+        isLoggedUserGroup(MANAGEMENT) &&
+        isLoggedUserInstitution(otherUserInstitutionUid) &&
+        otherUserGroupCode === MEMBER 
+      )
+    );
+}
+
+export default {
+  canCreateAnyUser: () => isLoggedUserGroup(SUPER_ADMIN),
+  canCreateMember: (newMemberGroupCode, newMemberInstitutionUid) => {
+    return canManageUserOrCreateMember(newMemberGroupCode, newMemberInstitutionUid);
+  },
+  canManageUser: (userGroupCode, userInstitutionUid) => {
+    return canManageUserOrCreateMember(userGroupCode, userInstitutionUid);
+  },
+  canChangeUserGroup: () => isLoggedUserGroup(SUPER_ADMIN),
+  canChangeUserInstitution: () => isLoggedUserGroup(SUPER_ADMIN),
+  canChangeOwnInstitution: () => isLoggedUserGroup(SUPER_ADMIN),
+  canCreateInstitutions: () => isLoggedUserGroup(SUPER_ADMIN),
+  canManageInstitution: (institutionUid) => {
+    return (
+      (
+        isLoggedUserGroup(SUPER_ADMIN)
+      ) || (
+        isLoggedUserGroup(MANAGEMENT) &&
+        isLoggedUserInstitution(institutionUid)
       )
     );
   },
-  // canManageInstitution: (institution) => {
-  //   return (
-  //     (
-  //       loggedUserGroupCode === "super-admin"
-  //     ) || (
-  //       loggedUserGroupCode === "management" && 
-  //       loggedUserInstitutionUid === institution.uid
-  //     )
-  //   );
-  // },
-  // canCreateInstitutions: () => {
-  //   return loggedUserGroupCode === "super-admin"; 
-  // },
-  // canManagePosts: () => {
-  //   return loggedUserGroupCode === "review"; 
-  // },
+  canManagePosts: () => isLoggedUserGroup(REVIEW),
 }
