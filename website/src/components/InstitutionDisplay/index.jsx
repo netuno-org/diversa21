9638d020ea9fb26089a3c043e791369d080b7197
RxposoT <raposopvptomas@gmail.com>
2026-05-22 17:20:39 +0100

    feat(frontend): add InstitutionDisplay component

---
+++ website/src/components/InstitutionDisplay/index.jsx
@@ -0,0 +1,74 @@
+import { Avatar } from 'antd';
+import {
+  EnvironmentOutlined,
+  GlobalOutlined,
+  MailOutlined,
+  PhoneOutlined
+} from '@ant-design/icons';
+
+import _service from '@netuno/service-client';
+
+import './index.less';
+
+function InstitutionDisplay({ institution, avatarStyle, children }) {
+  if (!institution) return null;
+
+  const avatarUrl = institution.logo
+    ? _service.url(`/institution/logo?uid=${institution.uid}`)
+    : null;
+
+  const location = [
+    institution.city,
+    institution.state,
+    institution.country
+  ].filter(Boolean).join(', ');
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', gap: 16 }}>
+      
+      <Avatar
+        style={avatarStyle}
+        src={avatarUrl}
+        shape="square"
+      >
+        {!avatarUrl && institution.name?.[0]}
+      </Avatar>
+
+      <div className="user-profile-display-content">
+
+        <div>
+          <strong>{institution.name}</strong>
+        </div>
+
+        {institution.email && (
+          <div>
+            <MailOutlined /> {institution.email}
+          </div>
+        )}
+
+        {institution.telephone && (
+          <div>
+            <PhoneOutlined /> {institution.telephone}
+          </div>
+        )}
+
+        {location && (
+          <div>
+            <EnvironmentOutlined /> {location}
+          </div>
+        )}
+
+        {institution.website && (
+          <div>
+            <GlobalOutlined /> {institution.website}
+          </div>
+        )}
+
+        {children}
+
+      </div>
+    </div>
+  );
+}
+
+export default InstitutionDisplay;
