eb206db9645e3e625a4217ed6c275a110bcbbe15
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-05-22 16:25:11 -0300

    refactor: extract constants

---
+++ server/core/lib/groups.js
@@ -0,0 +1,4 @@
+export const MEMBER = "member";
+export const MANAGEMENT = "management"; 
+export const REVIEW = "review"; 
+export const SUPER_ADMIN = "super-admin";
--- server/core/lib/permissions.js
+++ server/core/lib/permissions.js
@@ -1,12 +1,11 @@
 import people from "#core/lib/people.js";
-
-const MEMBER = "member";
+import { MEMBER, MANAGEMENT, REVIEW, SUPER_ADMIN } from "#core/lib/groups.js";
 
 const loggedUserGroupCode = _group.code();
 
-const isSuperAdmin = loggedUserGroupCode === "super-admin";
-const isManager = loggedUserGroupCode === "management"; 
-const isReview = loggedUserGroupCode === "review"; 
+const isManager = loggedUserGroupCode === MANAGEMENT; 
+const isReview = loggedUserGroupCode === REVIEW; 
+const isSuperAdmin = loggedUserGroupCode === SUPER_ADMIN; 
 
 const isLoggedUserInstitution = (institutionUid) => {
   const peopleUid = people.getLogged().getUID("uid");
