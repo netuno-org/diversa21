78a93145294f706ae1fef8fbe39e8b2eae147cf2
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-06-11 16:10:46 -0300

    test: add more tests to post list

--- server/__tests__/post/list/get.test.js
+++ server/__tests__/post/list/get.test.js
@@ -2,6 +2,7 @@ import request from "supertest";
 
 import toBePost from '../../custom/post.js';
 import login from '../../util/login.js';
+import { userUid, postUid } from '../../util/uids.js'
 import { NETUNO_URL } from '../../config.js';
 
 expect.extend({ toBePost });
@@ -29,4 +30,74 @@ test("list posts", async () => {
   expect(response.body.data.items[0]).toBePost();
   expect(response.body.data.items[0].liked).toBe(true);
   expect(response.body.data.items[1].liked).toBe(false);
+});
+
+it("shouldn't return comments if parent is not passed", async () => {
+  const accessToken = await login.asTest(); 
+
+  const response = await request(NETUNO_URL)
+    .get(`/post/list?peopleUid=${userUid.test}`)
+    .set("Authorization", `Bearer ${accessToken}`)
+    .expect(200);
+
+  expect(response.body.result).toBe(true);
+  expect(response.body).toHaveProperty("data");
+  expect(response.body.data.pageSize).toBe(10);
+  expect(typeof response.body.data.totalCount).toBe("number");
+  expect(response.body.data.totalCount).toBe(0);
+  expect(response.body.data).toHaveProperty("items");
+});
+
+test("list comments on a post", async () => {
+  const accessToken = await login.asTest(); 
+
+  const response = await request(NETUNO_URL)
+    .get(`/post/list?parent=${postUid.primeiroPost}`)
+    .set("Authorization", `Bearer ${accessToken}`)
+    .expect(200);
+
+  expect(response.body.result).toBe(true);
+  expect(response.body).toHaveProperty("data");
+  expect(response.body.data.pageSize).toBe(10);
+  expect(typeof response.body.data.totalCount).toBe("number");
+  expect(response.body.data.totalCount).toBe(2);
+  expect(response.body.data).toHaveProperty("items");
+  expect(response.body.data.items[0]).toBePost();
+  expect(response.body.data.items[0].content).toBe("eesh tarde demais");
+  expect(response.body.data.items[1].content).toBe("Primeiro comentário!");
+});
+
+test("list posts by a user", async () => {
+  const accessToken = await login.asTest(); 
+
+  const response = await request(NETUNO_URL)
+    .get(`/post/list?peopleUid=${userUid.ben}`)
+    .set("Authorization", `Bearer ${accessToken}`)
+    .expect(200);
+
+  expect(response.body.result).toBe(true);
+  expect(response.body).toHaveProperty("data");
+  expect(response.body.data.pageSize).toBe(10);
+  expect(typeof response.body.data.totalCount).toBe("number");
+  expect(response.body.data.totalCount).toBe(1);
+  expect(response.body.data).toHaveProperty("items");
+  expect(response.body.data.items[0]).toBePost();
+  expect(response.body.data.items[0].content).toBe("Primeiro post!");
+});
+
+test("list posts with pagination", async () => {
+  const accessToken = await login.asSuperAdmin(); 
+
+  const response = await request(NETUNO_URL)
+    .get("/post/list?page=2")
+    .set("Authorization", `Bearer ${accessToken}`)
+    .expect(200);
+
+  expect(response.body.result).toBe(true);
+  expect(response.body).toHaveProperty("data");
+  expect(response.body.data.pageSize).toBe(10);
+  expect(typeof response.body.data.totalCount).toBe("number");
+  expect(response.body.data.totalCount).toBe(0);
+  expect(response.body.data).toHaveProperty("items");
+  expect(response.body.data.items).toStrictEqual([]);
 });
--- server/__tests__/util/uids.js
+++ server/__tests__/util/uids.js
@@ -2,6 +2,7 @@ const userUid = {
   super: "e7ab1ade-d464-4602-bd61-f0f7eb7c880e",
   test: "2a86a611-2ab1-472d-a7fe-c41c4aeef36b",
   bob: "0abd451a-b951-4c95-adc9-96332ad6c772",
+  ben: "ceb1b82e-21ee-44cc-828e-fe1ea7ffb06d",
   notExist: "7c076702-0f99-44b9-b4f5-7b6d4810b7d8",
 }
 
@@ -15,4 +16,13 @@ const institutionUid = {
   laboratorioModelo: "8661840b-73d4-415c-bf87-984f1b4a6c42",
 }
 
-export { userUid, cityUid, institutionUid };
+const postUid = {
+ manutencaoAgendada: "f44e649a-987f-4c2c-9a83-d3ac370f82de",
+ primeiroComentario: "ab6dd7ae-9721-40ff-8dd1-0d89add93f8d",
+ iaDizerOMesmo: "c41d08d5-45ba-434b-a679-08b79a73b386",
+ eeshTardeDemais: "06fa908f-9fd8-441d-8a08-dd0278be2974",
+ comment21deMaio: "86635804-1264-4e58-a72a-94d12a75b158",
+ primeiroPost: "b3499f27-44b8-4169-92ea-d0a8b6c12148"
+}
+
+export { userUid, cityUid, institutionUid, postUid };
