01a9e3d962d6ce33ae5d844e010003929b979866
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-06-23 13:28:56 -0300

    feat: create job to send notifications

---
+++ server/services/jobs/notification.js
@@ -0,0 +1,29 @@
+import { _db } from "@netuno/server-types"
+
+// _log.info("Job de notificações")
+_db.execute(`
+    INSERT INTO notification (
+        id,
+        title,
+        content,
+        originator_id,
+        recipient_id,
+        sent_at,
+        read_at,
+        extra
+    )
+    SELECT
+        NEXTVAL('notification_id'),
+        'Novo post da sua instituição',
+        post.content,
+        originator.id,
+        recipient.id,
+        NOW(),
+        NULL,
+        ''
+    FROM post
+    INNER JOIN people originator ON post.people_id = originator.id
+    INNER JOIN people recipient ON recipient.institution_id = originator.institution_id
+    WHERE moment >= NOW() - INTERVAL '10 seconds'
+    ORDER BY moment DESC;
+`);
