d6969c1733ddfe1479d3ac8cb3e1cdd686b1c7b5
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-06-25 17:34:53 -0300

    feat(backend): notification for comment on my post

--- server/services/jobs/notification.js
+++ server/services/jobs/notification.js
@@ -92,9 +92,9 @@ _db.execute(`
         ON notification_opt_out.people_id = recipient.id
         AND notification_opt_out.type_id = ${friendRequestNotificationTypeId} 
     WHERE 1 = 1
-      AND request_at >= NOW() - INTERVAL '11 seconds'
-      AND originator.id <> recipient.id
-      AND notification_opt_out.id IS NULL
+        AND request_at >= NOW() - INTERVAL '11 seconds'
+        AND originator.id <> recipient.id
+        AND notification_opt_out.id IS NULL
     ORDER BY request_at DESC;
 `);
 
@@ -128,9 +128,9 @@ _db.execute(`
         ON notification_opt_out.people_id = recipient.id
         AND notification_opt_out.type_id = ${friendRequestAcceptedNotificationTypeId} 
     WHERE 1 = 1
-      AND accepted_at >= NOW() - INTERVAL '11 seconds'
-      AND originator.id <> recipient.id
-      AND notification_opt_out.id IS NULL
+        AND accepted_at >= NOW() - INTERVAL '11 seconds'
+        AND originator.id <> recipient.id
+        AND notification_opt_out.id IS NULL
     ORDER BY accepted_at DESC;
 `);
 
@@ -157,15 +157,55 @@ _db.execute(`
         '{ "postUid": "' || post.uid || '" }',
         ${institutionPostNotificationTypeId}
     FROM post
+    INNER JOIN post parent ON post.parent_id = parent.id 
     INNER JOIN people originator ON post.people_id = originator.id
-    INNER JOIN people recipient ON recipient.institution_id = originator.institution_id
+    INNER JOIN people recipient
+        ON recipient.institution_id = originator.institution_id
+        AND recipient.id <> parent.people_id
     INNER JOIN netuno_user originator_user ON originator.people_user_id = originator_user.id
     LEFT JOIN notification_opt_out
         ON notification_opt_out.people_id = recipient.id
         AND notification_opt_out.type_id = ${institutionPostNotificationTypeId} 
     WHERE 1 = 1
-      AND moment >= NOW() - INTERVAL '11 seconds'
-      AND originator.id <> recipient.id
-      AND notification_opt_out.id IS NULL
-    ORDER BY moment DESC;
+        AND post.moment >= NOW() - INTERVAL '11 seconds'
+        AND originator.id <> recipient.id
+        AND notification_opt_out.id IS NULL
+    ORDER BY post.moment DESC;
+`);
+
+_db.execute(`
+    INSERT INTO notification (
+        id,
+        title,
+        content,
+        originator_id,
+        recipient_id,
+        sent_at,
+        read_at,
+        extra,
+        type_id
+    )
+    SELECT
+        NEXTVAL('notification_id'),
+        '@' || originator_user.user AS originator_username,
+        'Comentou em um post seu.',
+        originator.id,
+        recipient.id,
+        NOW(),
+        NULL,
+        '{ "postUid": "' || comment.uid || '" }',
+        ${myPostCommentNotificationTypeId} 
+    FROM post comment
+    INNER JOIN post parent ON comment.parent_id = parent.id 
+    INNER JOIN people originator ON comment.people_id = originator.id
+    INNER JOIN people recipient ON recipient.id = parent.people_id
+    INNER JOIN netuno_user originator_user ON originator.people_user_id = originator_user.id
+    LEFT JOIN notification_opt_out
+        ON notification_opt_out.people_id = recipient.id
+        AND notification_opt_out.type_id = ${myPostCommentNotificationTypeId} 
+    WHERE 1 = 1
+        AND comment.moment >= NOW() - INTERVAL '11 seconds'
+        AND originator.id <> recipient.id
+        AND notification_opt_out.id IS NULL
+    ORDER BY comment.moment DESC;
 `);
