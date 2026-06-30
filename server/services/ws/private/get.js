560bb37d642b30039adb3cd1b4768b4c733b80f1
joaoInacio13 <joao.rm.inacio@gmail.com>
2026-06-30 14:55:17 +0100

    feat(ws): add private websocket services

---
+++ server/services/ws/private/get.js
@@ -0,0 +1,1 @@
+// GET method for stream, left empty as default listener pattern is used.
---
+++ server/services/ws/private/post.js
@@ -0,0 +1,13 @@
+import { _db, _ws } from "@netuno/server-types";
+import people from "#core/lib/people.js";
+
+const dbPeople = people.getLogged();
+
+if (dbPeople) {
+  _db.form("people_ws_session")
+    .set("people_id", dbPeople.getInt("id"))
+    .set("session_id", _ws.sessionId())
+    .insert();
+} else {
+  _ws.close();
+}
