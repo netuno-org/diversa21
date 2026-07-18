a6024191832e38adb7eb0d05d0cee6ac91c724cd
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2026-07-18 14:05:17 +0100

    feat(backend): openapi for messages

--- server/services/message/unread/count/get.js
+++ server/services/message/unread/count/get.js
@@ -2,10 +2,10 @@ import { _val, _out } from "@netuno/server-types";
 
 import people from "#core/lib/people.js";
 import message from "#core/lib/message.js";
+import response from "#core/lib/response.js";
 
 const dbPeopleLogged = people.getLogged();
 
-_out.json(
-  _val.map()
-    .set("total", message.getUnreadTotal(dbPeopleLogged))
-);
+const data = message.getUnreadTotal(dbPeopleLogged);
+
+response.successWithData(data);
