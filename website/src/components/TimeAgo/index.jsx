e05b1c8c14b63798f6b1fd675a3c384be7055b92
Thiago da Silva Pinto <thiago.dev2@gmail.com>
2026-07-20 18:19:21 -0300

    fix(TimeAgo): refresh TimeAgo on new message timestamp

--- website/src/components/TimeAgo/index.jsx
+++ website/src/components/TimeAgo/index.jsx
@@ -53,7 +53,7 @@ function TimeAgo({ sentAt, className = '' }) {
     onTick();
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [sentAt]);
 
   return (
     <span className={['time-ago', className].join(' ')}>
