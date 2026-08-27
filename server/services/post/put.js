601bcf17230fb3989dbecbd93804bb3bffb3dd7a
Thiago da Silva Pinto <thiago.dev2@gmail.com>
2026-08-27 13:59:30 -0300

    fix: allow super admin and review to edit other users posts

--- server/services/post/put.js
+++ server/services/post/put.js
@@ -1,4 +1,5 @@
 import {_req, _db, _val, _user, _header, _exec, _out} from "@netuno/server-types"
+import permissions from "#core/lib/permissions.js";
 
 // TODO: import response from "#core/lib/response.js";
 
@@ -6,16 +7,23 @@ const uid = _req.getString("uid");
 const content = _req.getString("content");
 
 const dbPost = _db.queryFirst(`
-    SELECT post.id
+    SELECT post.id, people.people_user_id
     FROM post
         INNER JOIN people ON post.people_id = people.id
-    WHERE 1 = 1
-        AND post.uid = ?::uuid
-        AND people.people_user_id = ?::int 
-`, uid, _user.id);
+    WHERE post.uid = ?::uuid
+`, uid);
 
 if (!dbPost) {
-  _header.status(400);
+  _header.status(404);
+  _exec.stop();
+}
+
+if (dbPost.getInt("people_user_id") !== _user.id && !permissions.canManagePosts()) {
+  _header.status(403);
+  _out.json(
+    _val.map()
+      .set("error", "permission denied")
+  );
   _exec.stop();
 }
 
